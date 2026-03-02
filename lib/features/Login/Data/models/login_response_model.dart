aff03686927a0a57cdf8d73efda9f50f36374473
ZIAD <ziad.abdelalim230@gmail.com>
2026-03-03 01:56:28 +0200

    Add LoginResponseModel with token and user structure

---
+++ lib/features/Login/Data/models/login_response_model.dart
@@ -0,0 +1,18 @@
+import 'user_model.dart';
+
+class LoginResponseModel {
+  final String token;
+  final UserModel user;
+
+  LoginResponseModel({
+    required this.token,
+    required this.user,
+  });
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) {
+    return LoginResponseModel(
+      token: json['token'],
+      user: UserModel.fromJson(json['user']),
+    );
+  }
+}
