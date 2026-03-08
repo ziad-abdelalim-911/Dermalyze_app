7651b0da18ce19a920ebb147a9119961371123e0
ZIAD <ziad.abdelalim230@gmail.com>
2026-03-08 03:44:49 +0200

    feat: update bottomNavBar route to support isDoctor argument

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,7 @@
+import 'package:dermalyze/features/auth/view/home/doctor/screens/all_patients_screen.dart';
+import 'package:dermalyze/features/auth/view/home/doctor/screens/critical_patients_screen.dart';
+import 'package:dermalyze/features/auth/view/home/doctor/screens/doctor_home_screen.dart';
+import 'package:dermalyze/features/auth/view/home/doctor/screens/doctor_profile_screen.dart';
 import 'package:dermalyze/features/auth/view/login/Patient_SignUp.dart';
 import 'package:dermalyze/features/auth/view/login/doctor_SignUp.dart';
 import 'package:dermalyze/features/auth/view/login/register_view.dart';
@@ -5,6 +9,7 @@ import 'package:dermalyze/features/auth/view/login/signup_view.dart';
 import 'package:dermalyze/features/auth/view/navigation/custom_bottom_nav_bar.dart';
 import 'package:dermalyze/features/auth/view/on_boarding/on_boarding_view.dart';
 import 'package:dermalyze/features/auth/view/patients/screens/add_new_patient_screen.dart';
+import 'package:dermalyze/features/auth/view/patients/screens/patient_details_screen.dart';
 import 'package:dermalyze/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -48,7 +53,10 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
 
       case AppRoutes.bottomNavBar:
-        return MaterialPageRoute(builder: (_) => const CustomBottomNavBar());
+        final isDoctor = settings.arguments as bool? ?? false;
+        return MaterialPageRoute(
+          builder: (_) => CustomBottomNavBar(isDoctor: isDoctor),
+        );
 
       case AppRoutes.progressReport:
         return MaterialPageRoute(builder: (_) => const ProgressReportView());
@@ -65,8 +73,27 @@ class AppRouter {
       case AppRoutes.addNewPatient:
         return MaterialPageRoute(builder: (_) => const AddNewPatientScreen());
 
+      case AppRoutes.patientDetails:
+        return MaterialPageRoute(builder: (_) => const PatientDetailsScreen());
+
+      case AppRoutes.doctorHome:
+        return MaterialPageRoute(builder: (_) => const DoctorHomeScreen());
+
+      case AppRoutes.allPatients:
+        return MaterialPageRoute(builder: (_) => const AllPatientsScreen());
+
+      case AppRoutes.criticalPatients:
+        return MaterialPageRoute(
+          builder: (_) => const CriticalPatientsScreen(),
+        );
+
+      case AppRoutes.doctorProfile:
+        return MaterialPageRoute(builder: (_) => const DoctorProfileScreen());
+
       default:
-        return MaterialPageRoute(builder: (_) => const CustomBottomNavBar());
+        return MaterialPageRoute(
+          builder: (_) => const CustomBottomNavBar(isDoctor: false),
+        );
     }
   }
 }
